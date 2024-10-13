3a6535b7956c3d37f995d8bd0e0160907b123955
Svitlana Riabtseva <161156007+Svitlana-Riabtseva@users.noreply.github.com>
2024-10-13 16:39:38 +0300

    add onImageClick

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,9 +1,16 @@
-const ImageCard = ({ imageSrc, imageAlt }) => {
+const ImageCard = ({ imageSrc, imageAlt, onImageClick }) => {
   return (
-		<div>
-		  <img src={imageSrc} alt={imageAlt} width="400px" height="266px"/>
-		</div>
-  )
+    <div>
+      <img 
+        src={imageSrc} 
+        alt={imageAlt} 
+        width="400px" 
+        height="266px" 
+        onClick={onImageClick}  // Обробник події кліку
+        style={{ cursor: "pointer" }}  // Стиль курсора для зручності
+      />
+    </div>
+  );
 }
 
-export default ImageCard
+export default ImageCard;
